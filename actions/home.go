224af89db4da70dbbbecc571a97a94085095b8cb
Emanuel Bennici <eb@fabmation.de>
2019-11-21 18:31:04 +0100

    Add route- & 500-Error-Template

--- actions/home.go
+++ actions/home.go
@@ -12,7 +12,7 @@ func HomeHandler(c buffalo.Context) error {
 // this route is only for Developing
 func RoutesHandler(c buffalo.Context) error {
 	// check if we are in development Mode
-	if ENV != "development" {
+	if ENV == "development" {
 		return c.Render(200, r.HTML("routes.html"))
 	} else {
 		return c.Render(500, r.HTML("null.html", "_500.html"))
