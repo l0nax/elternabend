6b7aaeb0eae9ac8f80cb73096f4f39565d45697b
Emanuel Bennici <eb@fabmation.de>
2019-11-27 16:44:12 +0100

    Add MDBootstrap to application.(js|scss)

--- assets/js/application.js
+++ assets/js/application.js
@@ -1,6 +1,9 @@
 require("expose-loader?$!expose-loader?jQuery!jquery");
 require("bootstrap/dist/js/bootstrap.bundle.js");
 require("bootstrap-datetime-picker/js/bootstrap-datetimepicker.min.js");
+// required by bootstrap and mdbootstrap
+require("popper.js/dist/popper.min.js");
+require("mdbootstrap/js/mdb.min.js");
 
 $(() => {
 
