521975c6ae9acba82c381fe8a4f0b7ad5838d555
Emanuel Bennici <eb@fabmation.de>
2020-01-20 15:43:58 +0100

    Update bootstrap-datetimepicker component

--- assets/js/application.js
+++ assets/js/application.js
@@ -1,10 +1,11 @@
 require("expose-loader?$!expose-loader?jQuery!jquery");
 require("bootstrap/dist/js/bootstrap.bundle.js");
-require("bootstrap-datetime-picker/js/bootstrap-datetimepicker.min.js");
-// required by bootstrap and mdbootstrap
 require("popper.js/dist/popper.min.js");
+// required by bootstrap and mdbootstrap
 require("mdbootstrap/js/mdb.min.js");
 
 $(() => {
 
 });
+
+
