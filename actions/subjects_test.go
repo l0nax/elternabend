3d85fdfffd7c61ea441c2ab66d3a55116256ff93
Emanuel Bennici <eb@fabmation.de>
2020-01-20 11:34:21 +0100

    Add 'subjects' model

---
+++ actions/subjects_test.go
@@ -0,0 +1,36 @@
+package actions
+
+import (
+  "testing"
+
+  "github.com/stretchr/testify/require"
+)
+
+func (as *ActionSuite) Test_SubjectsResource_List() {
+  as.Fail("Not Implemented!")
+}
+
+func (as *ActionSuite) Test_SubjectsResource_Show() {
+  as.Fail("Not Implemented!")
+}
+
+func (as *ActionSuite) Test_SubjectsResource_Create() {
+  as.Fail("Not Implemented!")
+}
+
+func (as *ActionSuite) Test_SubjectsResource_Update() {
+  as.Fail("Not Implemented!")
+}
+
+func (as *ActionSuite) Test_SubjectsResource_Destroy() {
+  as.Fail("Not Implemented!")
+}
+
+func (as *ActionSuite) Test_SubjectsResource_New() {
+  as.Fail("Not Implemented!")
+}
+
+func (as *ActionSuite) Test_SubjectsResource_Edit() {
+  as.Fail("Not Implemented!")
+}
+
